f876f992a8a7fcdb6fe635a2991ae5b7bdc6fe76
Dylanotina <47347233+Dylanotina@users.noreply.github.com>
2019-03-20 10:43:59 +0100

    ajout function autocomplete

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,25 @@
-sdfsdfsd
+$(document).ready(function(){
+    var cherhche = $("#commune");
+    cherhche.autocomplete({
+        minLength :3,
+        ville_startsWith : cherhche.val(),
+        source : function(request,response){
+            $.ajax({
+                url : 'http://localhost/tests/commune.php',
+                Type:"GET",
+                data: 'commune='+ cherhche.val(),
+                dataType:'json',
+                success: function(data){
+                    response($.map(data,function(item){
+                        return item.Ville
+                    }))
+                },
+                select : function(event,ui){
+                    var paragraphe = $("div");
+                    alert(ui.item.Ville);
+                    return false;
+
+                }})
+        },
+    })
+});
