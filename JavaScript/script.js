6c1558bb762c775b62bd557d9ebbaaa807b38c82
Dylanotina <47347233+Dylanotina@users.noreply.github.com>
2019-03-20 11:25:53 +0100

    Ajout fonction autocomplete

---
+++ JavaScript/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    var cherhche = $("#commune");
+    cherhche.autocomplete({
+        minLength :3,
+        ville_startsWith : cherhche.val(),
+        source : function(request,response){
+            $.ajax({
+                url : 'http://localhost/tests/ProjetJS/commune.php',
+                Type:"GET",
+                data: 'commune='+ cherhche.val(),
+                dataType:'json',
+                success: function(data){
+                    response($.map(data,function(item){
+                        return item.Ville
+                    }))
+                },
+                select : function(event,ui){
+                    var paragraphe = $("div");
+                    alert(ui.item.Ville);
+                    return false;
+
+                }})
+        },
+    })
+});
